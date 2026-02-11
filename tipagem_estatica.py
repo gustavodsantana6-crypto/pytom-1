28473b6a744f5928d1a291703abc33d2aee1d9dd
gustavodsantana6-crypto <gustavo.d.santana6@ba.estudante.senai.br>
2026-02-11 16:53:48 -0300

    Add files via upload

---
+++ tipagem_estatica.py
@@ -0,0 +1,16 @@
+# TIPAGEM ESTÁTICA
+
+nome: str
+idade: int
+altura: float
+marriculado: bool
+
+nome= "Marta"
+idade= 23
+altura= 1.70
+matriculado= True
+
+print("Nome: ", nome)
+print("idade: ", idade)
+print("Altura ", altura )
+print("Matriculado ", matriculado)
